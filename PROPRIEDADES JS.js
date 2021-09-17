bd5a48485f64cceee2f7a3c9ecba97907a27a1aa
Diego Br <88857178+DiegoBr7@users.noreply.github.com>
2021-09-17 20:47:11 -0300

    Add files via upload

---
+++ PROPRIEDADES JS.js
@@ -0,0 +1,12 @@
+var aluno = {nomer = "diego" ,
+            nota : 7.5 
+}
+console.log(aluno[nome]) ;
+console.log(aluno.nota[1]);
+console.log(aluno.nota[0]) ;
+
+
+var novaProp = "lastName" ;
+aluno.matricula = 12345 ;
+aluno[novaProp] = "oliveira" ; 
+console. log (aluno)
